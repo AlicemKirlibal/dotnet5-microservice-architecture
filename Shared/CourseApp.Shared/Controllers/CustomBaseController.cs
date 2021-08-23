6329fbd74065052f2d7805cda67d119f4a553988
alicem.kirlibal <alicem.kirlibal@tsoft.com.tr>
2021-08-24 00:30:46 +0300

    added custombase controller for general statuscode

---
+++ Shared/CourseApp.Shared/Controllers/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using CourseApp.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseApp.Shared.Controllers
+{
+    public class CustomBaseController:ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
