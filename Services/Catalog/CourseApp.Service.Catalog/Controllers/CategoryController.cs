ff5e50a4ab3d77b75032802317e0939c27d3e547
alicem.kirlibal <alicem.kirlibal@tsoft.com.tr>
2021-08-24 00:31:16 +0300

    added category  controller(endpoints)

---
+++ Services/Catalog/CourseApp.Service.Catalog/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using CourseApp.Service.Catalog.Dtos;
+using CourseApp.Service.Catalog.Services;
+using CourseApp.Shared.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Service.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _categoryService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryCreateDto createDto)
+        {
+            var response = await _categoryService.CreateAsync(createDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CategoryDto categoryDto)
+        {
+            var response = await _categoryService.UpdateAsync(categoryDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+
+            var response = await _categoryService.DeleteAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+
+
+
+
+
+
+    }
+}
