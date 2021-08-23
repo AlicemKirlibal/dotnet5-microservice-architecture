80f77f7ebf5dfab21fd2c3b08a7ecb69a6ae5e75
alicem.kirlibal <alicem.kirlibal@tsoft.com.tr>
2021-08-23 03:17:04 +0300

    added course services

---
+++ Services/Catalog/CourseApp.Service.Catalog/Services/ICourseService.cs
@@ -0,0 +1,18 @@
+﻿using CourseApp.Service.Catalog.Dtos;
+using CourseApp.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Service.Catalog.Services
+{
+    public interface ICourseService
+    {
+        Task<Response<List<CourseDto>>> GetAllAsync();
+        Task<Response<CourseDto>> GetByIdAsync(string id);
+        Task<Response<CourseDto>> CreateAsync(CourseCreateDto createDto);
+        Task<Response<NoContent>> UpdateAsync(CourseUpdateDto updateDto);
+        Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
--- Services/Catalog/CourseApp.Service.Catalog/Startup.cs
+++ Services/Catalog/CourseApp.Service.Catalog/Startup.cs
@@ -1,4 +1,5 @@
 using CourseApp.Service.Catalog.DbSettings;
+using CourseApp.Service.Catalog.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +28,9 @@ namespace CourseApp.Service.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<ICourseService, CourseService>();
+
             services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
 
