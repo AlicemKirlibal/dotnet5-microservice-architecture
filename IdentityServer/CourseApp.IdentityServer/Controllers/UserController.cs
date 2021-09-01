2bb0de8b683f1e51b5d83a5004e2f3ba2680a72a
alicem.kirlibal <alicem.kirlibal@tsoft.com.tr>
2021-09-01 16:10:40 +0300

    identtiyserver and user operations added

---
+++ IdentityServer/CourseApp.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using CourseApp.IdentityServer.Dtos;
+using CourseApp.IdentityServer.Models;
+using CourseApp.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace CourseApp.IdentityServer.Controllers
+{   [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                City = signUpDto.City,
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user,signUpDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
+            }
+
+            return NoContent();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaims = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaims == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaims.Value);
+            if (user == null) return BadRequest();
+
+            return Ok(new {Id=user.Id,UserName=user.UserName,Email=user.Email,City=user.City});
+           
+        }
+
+
+
+    }
+}
---
+++ IdentityServer/CourseApp.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,50 @@
+﻿using CourseApp.IdentityServer.Models;
+using IdentityModel;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var userExist = await _userManager.FindByEmailAsync(context.UserName);
+
+            if (userExist==null)
+            {
+                var errors = new Dictionary<string, object>();
+
+                errors.Add("errors",new List<string> {"Email or Password error" });
+
+                context.Result.CustomResponse = errors;
+                return;
+            }
+
+            var passwordCheck = await _userManager.CheckPasswordAsync(userExist,context.Password);
+            if (passwordCheck == false)
+            {
+                var errors = new Dictionary<string, object>();
+
+                errors.Add("errors", new List<string> { "Email or Password error" });
+
+                context.Result.CustomResponse = errors;
+                return;
+            }
+
+            context.Result = new GrantValidationResult(userExist.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
+
+        }
+    }
+}
