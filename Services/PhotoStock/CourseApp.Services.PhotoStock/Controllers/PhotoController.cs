39c7988706b04a06a181bb573484234b1604e2b8
alicem.kirlibal <alicem.kirlibal@tsoft.com.tr>
2021-09-05 14:39:09 +0300

    added photosave and delete apis

---
+++ Services/PhotoStock/CourseApp.Services.PhotoStock/Controllers/PhotoController.cs
@@ -0,0 +1,59 @@
+﻿using CourseApp.Services.PhotoStock.Dtos;
+using CourseApp.Shared.Controllers;
+using CourseApp.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CourseApp.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : CustomBaseController
+    {
+        public async Task<IActionResult> PhotoSave(IFormFile photo,CancellationToken cancellationToken)
+        {
+            if (photo!=null&&photo.Length>0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+
+                using var stream = new FileStream(path, FileMode.Create);
+
+                await photo.CopyToAsync(stream, cancellationToken);
+
+                //photostock.api/photos/forexample.jpg
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new PhotoDto { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto,200));
+
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+
+
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+
+
+        }
+    }
+}
--- Services/PhotoStock/CourseApp.Services.PhotoStock/Startup.cs
+++ Services/PhotoStock/CourseApp.Services.PhotoStock/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,17 @@ namespace CourseApp.Services.PhotoStock
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_photo_stock_catalog";
+                options.RequireHttpsMetadata = false;
+            });
+
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter());
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CourseApp.Services.PhotoStock", Version = "v1" });
@@ -43,8 +55,9 @@ namespace CourseApp.Services.PhotoStock
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CourseApp.Services.PhotoStock v1"));
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
