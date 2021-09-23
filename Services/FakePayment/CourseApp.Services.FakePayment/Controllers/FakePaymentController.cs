589c36031edb9394ed3dc2b97484f42993c2985e
alicem.kirlibal@tsoft.com.tr <alicem.kirlibal@tsoft.com.tr>
2021-09-23 18:55:50 +0300

    added fake payment

---
+++ Services/FakePayment/CourseApp.Services.FakePayment/Controllers/FakePaymentController.cs
@@ -0,0 +1,24 @@
+﻿using CourseApp.Shared.Controllers;
+using CourseApp.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentController : CustomBaseController
+    {
+
+
+        [HttpPost]
+        public  IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
