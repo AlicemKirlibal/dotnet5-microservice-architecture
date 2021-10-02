0211c119063844e82c20f52b316d395b317ec5d9
alicem.kirlibal@tsoft.com.tr <alicem.kirlibal@tsoft.com.tr>
2021-10-03 02:11:54 +0300

    added basket view models and IBasketService

---
+++ Frontends/CourseApp.Web/Services/Abstract/IBasketService.cs
@@ -0,0 +1,25 @@
+﻿using CourseApp.Web.Models.Baskets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Services.Abstract
+{
+   public interface IBasketService
+    {
+        Task<bool> SaveOrUpdate(BasketViewModel viewModel);
+
+        Task<BasketViewModel> Get();
+
+        Task<bool> Delete();
+
+        Task AddBasketItem(BasketItemViewModel viewModel);
+
+        Task<bool> RemoveBasketItem(string courseId);
+
+        Task<bool> ApplyDiscount(string discountCode);
+
+        Task<bool> CancelApplyDiscount();
+    }
+}
