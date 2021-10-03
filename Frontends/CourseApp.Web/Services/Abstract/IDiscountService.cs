c273b523055ab5d60da6943d1c5e61891d4ac291
alicem.kirlibal@tsoft.com.tr <alicem.kirlibal@tsoft.com.tr>
2021-10-03 21:03:23 +0300

    added discount service

---
+++ Frontends/CourseApp.Web/Services/Abstract/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using CourseApp.Web.Models.Discounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Services.Abstract
+{
+   public interface IDiscountService
+    {
+        Task<DiscountViewModel> GetDiscount(string discountCode);
+    }
+}
---
+++ Frontends/CourseApp.Web/Services/Concrete/DiscountService.cs
@@ -0,0 +1,38 @@
+﻿using CourseApp.Shared.Dtos;
+using CourseApp.Web.Models.Discounts;
+using CourseApp.Web.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Services.Concrete
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DiscountService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            var response = await _httpClient.GetAsync($"discount/GetByCode/{discountCode}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var discount = await response.Content.ReadFromJsonAsync<Response<DiscountViewModel>>();
+
+            return discount.Data;
+
+
+        }
+    }
+}
