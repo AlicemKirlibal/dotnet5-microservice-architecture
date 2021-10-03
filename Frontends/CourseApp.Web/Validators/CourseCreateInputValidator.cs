08920b0d75a07ca7ae04564caad2989969ea728e
alicem.kirlibal@tsoft.com.tr <alicem.kirlibal@tsoft.com.tr>
2021-10-03 17:26:45 +0300

    Added fluentValidation for course update and create models

---
+++ Frontends/CourseApp.Web/Validators/CourseCreateInputValidator.cs
@@ -0,0 +1,22 @@
+﻿using CourseApp.Web.Models.Catalogs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Validators
+{
+    public class CourseCreateInputValidator:AbstractValidator<CreateCourseViewModel>
+    {
+        public CourseCreateInputValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("isim alanı boş bırakılamaz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("description alanı boş bırakılamaz");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("fiyat alanı boş bırakılamaz");
+            RuleFor(x => x.Feature.Duration).NotEmpty().WithMessage("süre alanı boş bırakılamaz");
+            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("kategori adı seciniz");
+        }
+
+    }
+}
