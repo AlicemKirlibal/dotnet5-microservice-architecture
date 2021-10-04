c3f37a3132ff1930a9d2d1f3cf463f8681fb462a
alicem.kirlibal@tsoft.com.tr <alicem.kirlibal@tsoft.com.tr>
2021-10-04 13:41:39 +0300

    added order services

---
+++ Frontends/CourseApp.Web/Services/Abstract/IOrderService.cs
@@ -0,0 +1,27 @@
+﻿using CourseApp.Web.Models.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Services.Abstract
+{
+   public interface IOrderService
+    {
+        /// <summary>
+        /// Senkron iletişim- direk order mikroservisine istek yapılacak
+        /// </summary>
+        /// <param name="checkoutInfoInput"></param>
+        /// <returns></returns>
+        Task<OrderCreatedViewModel> CreateOrder(CheckOutInfoInput checkoutInfoInput);
+
+        /// <summary>
+        /// Asenkron iletişim- sipariş bilgileri rabbitMQ'ya gönderilecek
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        Task<OrderSuspendViewModel> SuspendOrder(CheckOutInfoInput checkoutInfoInput);
+
+        Task<List<OrderViewModel>> GetOrder();
+    }
+}
