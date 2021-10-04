a162566f509dd3245b61845642e17d68b7c534fe
alicem.kirlibal@tsoft.com.tr <alicem.kirlibal@tsoft.com.tr>
2021-10-04 13:42:14 +0300

    added payment services

---
+++ Frontends/CourseApp.Web/Services/Abstract/IPaymentService.cs
@@ -0,0 +1,13 @@
+﻿using CourseApp.Web.Models.Fakepayments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Services.Abstract
+{
+   public interface IPaymentService
+    {
+        Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput);
+    }
+}
