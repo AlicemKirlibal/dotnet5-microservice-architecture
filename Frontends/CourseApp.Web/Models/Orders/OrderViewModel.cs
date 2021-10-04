db8af30249f3c72ca9f4bad0195583d1da0d8be4
alicem.kirlibal@tsoft.com.tr <alicem.kirlibal@tsoft.com.tr>
2021-10-04 13:41:15 +0300

    added order models

---
+++ Frontends/CourseApp.Web/Models/Orders/OrderViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Models.Orders
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        //public AddressDto Address { get; set; }
+
+        public string BuyerId { get; set; }
+
+        public List<OrderItemViewModel> OrderItemDtos { get; set; }
+    }
+}
